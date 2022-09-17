bcbd203008931e13d09af2c214cb20cc11a6239a
Sergei Deryglazov <6339878-deryglazov@users.noreply.gitlab.com>
2022-09-17 14:57:01 +0300

    init2

---
+++ RaleWears.Android/CustomUrlSchemeInterceptorActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using RaleWears.AuthHelpers;
+
+namespace RaleWears.Droid
+{
+	[Activity(Label = "CustomUrlSchemeInterceptorActivity", NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
+	[IntentFilter(
+	new[] { Intent.ActionView },
+	Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+	DataSchemes = new[] { "com.googleusercontent.apps.844060696235-ltgivjolb8v7ioidint435qa2o8ls38d" },
+	DataPath = "/oauth2redirect")]
+	public class CustomUrlSchemeInterceptorActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Convert Android.Net.Url to Uri
+			var uri = new Uri(Intent.Data.ToString());
+
+			// Load redirectUrl page
+			AuthenticationState.Authenticator.OnPageLoading(uri);
+
+			Finish();
+		}
+	}
+}
